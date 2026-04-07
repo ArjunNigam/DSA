e5d5ef1e895b54a3bf7dfe29cc7593a6c337bcc9
Arjun Nigam <arjunnigam.1713@gmail.com>
2026-04-07 20:25:00 +0000

    Next Greater Element

---
+++ Stacks/Next Greater Element/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] nextGreater(int[] A) {
+        int n = A.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[n];
+        for(int i = n-1;i>=0;i--)
+        {
+            while(!stack.isEmpty() && A[stack.peek()] <= A[i])
+            {
+                stack.pop();
+            }
+            if(stack.isEmpty())
+            {
+                result[i] = -1;
+            }
+            else 
+            {
+                result[i] = A[stack.peek()];
+            }
+            stack.push(i);
+        }
+        return result;
+
+    }
+}
